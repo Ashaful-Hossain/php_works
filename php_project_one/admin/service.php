f18d20150a467b90fd3a22f3f82ea2e8306436ce
Ashraf <miazi.ashraf@gmail.com>
2024-01-08 23:59:47 +0600

    new file add

--- php_project_one/admin/service.php
+++ php_project_one/admin/service.php
@@ -1,21 +1,19 @@
 <?php
 session_start();
 $db = new mysqli('localhost','root','','courier_service');
-$data = $db->query("select * from sliders order by id desc");
+$data = $db->query("select * from services order by id desc");
 
 if(!isset($_SESSION['username'])){
     header('Location:index.php');
 }
-if(isset($_POST['title_1'])){
-    $title_1 = $_POST['title_1'];
-    $title_2 = $_POST['title_2'];
-    $title_3 = $_POST['title_3'];
-    $brief = $_POST['brief'];
-    $photo = $_FILES['photo']['name'];
-    move_uploaded_file($_FILES['photo']['tmp_name'],'../uploads/'.$photo);
- $query ="INSERT INTO `sliders` (`id`, `title_1`, `title_2`, `title_3`, `brief`, `photo`) VALUES (NULL,'$title_1','$title_2','$title_3','$brief','$photo')";
+if(isset($_POST['title'])){
+    $title  = $_POST['title'];
+    $description = $_POST['description'];
+    $logo = $_FILES['logo']['name'];
+    move_uploaded_file($_FILES['logo']['tmp_name'],'../uploads/'.$logo);
+ $query ="INSERT INTO `services` (`id`, `logo`, `title`, `description`) VALUES (NULL,'$logo','$title','$description')";
  $data = $db->query($query);
- header('Location: slider.php
+ header('Location: service.php
  ');
 }
 ?>
@@ -36,28 +34,20 @@ if(isset($_POST['title_1'])){
 <div class="row">
 <div class="col-6">
 <div class="form-group">
-<label for="t-1">Title One</label>
-<input type="text" name="title_1" class="form-control" id="t-1" placeholder="Title one">
+<label for="t-1">Title</label>
+<input type="text" name="title" class="form-control" id="t-1" placeholder="Title one">
 </div>
 <div class="form-group">
-<label for="t-2">Title Two</label>
-<input type="text" name="title_2" class="form-control" id="t-2" placeholder="Title Two">
-</div>
-<div class="form-group">
-<label for="t-3">Title Three</label>
-<input type="text" name="title_3" class="form-control" id="t-3" placeholder="Title Three">
+    <label for="br">Description</label>
+    <input type="text" name="description" class="form-control" id="br" placeholder="Brief">
 </div>
 </div>
 <div class="col-6">
 <div class="form-group">
-<label for="br">Brief</label>
-<input type="text" name="brief" class="form-control" id="br" placeholder="Brief">
-</div>
-<div class="form-group">
-<label for="exampleInputFile">File input</label>
+<label for="exampleInputFile">Logo input</label>
 <div class="input-group">
 <div class="custom-file">
-<input type="file" class="custom-file-input" name="photo" id="exampleInputFile">
+<input type="file" class="custom-file-input" name="logo" id="exampleInputFile">
 <label class="custom-file-label" for="exampleInputFile">Choose file</label>
 </div>
 <div class="input-group-append">
@@ -85,7 +75,7 @@ if(isset($_POST['title_1'])){
     <div class="container">
     <div class="card card-success">
 <div class="card-header">
-<h3 class="card-title">Slider List</h3>
+<h3 class="card-title">Services List</h3>
 </div>
 <!-- Main content -->
        <div class="card-body">
@@ -93,30 +83,26 @@ if(isset($_POST['title_1'])){
        <table class="table table-borderd">
         <tr>
           <th>SL</th>
-          <th>Title One</th>
-          <th>Title Two</th>
-          <th>Title Three</th>
-          <th>Brief</th>
-          <th>Photo</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Logo</th>
           <th>Action</th>
         </tr>
         <?php 
         $i=0;
-        while($slider = $data->fetch_assoc()) {
+        while($ser = $data->fetch_assoc()) {
           ?>
         <tr>
 
           <td><?php echo ++$i ?></td>
-          <td><?php echo $slider['title_1'] ?></td>
-          <td><?php echo $slider['title_2'] ?></td>
-          <td><?php echo $slider['title_3'] ?></td>
-          <td><?php echo $slider['brief'] ?></td>
-          <td><img src="../uploads/<?php echo $slider['photo'] ?>" alt=""
+          <td><?php echo $ser['title'] ?></td>
+          <td><?php echo $ser['description'] ?></td>
+          <td><img src="../uploads/<?php echo $ser['logo'] ?>" alt=""
           width="80" height="50"></td>
 
           <td>
-            <a href="edit_slider.php?id=<?php echo $slider['id'] ?>" class="btn btn-success">Edit</a>
-            <a href="delete_slider.php?id=<?php echo $slider['id'] ?>" class="btn btn-danger">Delete</a>
+            <a href="edit_slider.php?id=<?php echo $ser['id'] ?>" class="btn btn-success">Edit</a>
+            <a href="delete_slider.php?id=<?php echo $ser['id'] ?>" class="btn btn-danger">Delete</a>
           </td>
         </tr>
         <?php }?>
