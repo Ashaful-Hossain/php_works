ef56f4f032e738a2ae28ef4ae7eeb7236d63f5cb
Ashraf <miazi.ashraf@gmail.com>
2024-01-17 23:59:46 +0600

    courier web-site

---
+++ php_project_one/courier-service/contact.php
@@ -0,0 +1,204 @@
+
+<?php require('header.php'); ?>
+    <!--==========Header Banner Start==============-->
+    <div class="cs_pagetitle">
+        <div class="cs_img_overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="page_title">
+                        <h2>Contact Us</h2>
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <ul class="breadcrumb">
+                        <li><a href="index.html">home</a></li>
+                        <li>//</li>
+                        <li><a href="about.html">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--========Header Banner End===============-->
+    <!--========Contact Form Start===============-->
+    <div class="cs_contact_form_wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+                    <div class="cs_contact_form wow fadeInLeft" data-wow-delay="0.1s">
+                        <h4 class="cs_sub_heading relative">Send Us A Messages</h4>
+                        <form>
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                                    <div class="cs_field_holder">
+                                        <input type="text" class="cs_form_field require" placeholder="Enter Your Name" name="full_name" id="full_name">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                                    <div class="cs_field_holder">
+                                        <input type="text" class="cs_form_field require" placeholder="Enter Your Email" name="email" id="email" data-valid="email" data-error="Email should be valid.">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                                    <div class="cs_field_holder">
+                                        <input type="text" class="cs_form_field require" placeholder="Enter Your Subject" name="subject" id="subject">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+                                    <div class="cs_field_holder">
+                                        <input type="text" class="cs_form_field" placeholder="Enter Your Phone">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <div class="cs_field_holder">
+                                        <textarea placeholder="Enter Your Message..." class="cs_form_field require" name="message" id="message"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                                    <button type="button" class="cs_main_btn submitForm">Submit</button>
+                                    <div class="response"></div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!--===================Map Section==================-->
+                <div class="col-lg-6 col-md-12 col-sm-12 col-12">
+                    <div class="cs_map_wrapper wow fadeInRight" data-wow-delay="0.1s">
+                        <div class="container-fluid">
+                            <div class="cs_map_section">
+                                <div><iframe height="600" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwcs=B&amp;output=embed"></iframe>
+                                    <a href="https://www.maps.ie/route-planner.htm">Driving Route Planner</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+     <!--===================Contact Section==================-->
+     <div class="cs_contact_wrapper wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+                    <div class="cs_contact_box" data-wow-duration="1.3s" style="visibility: visible; animation-duration: 0.8s; animation-name: slideInLeft;">
+                        <img src="assets/images/index/time.svg" class="img-fluid" alt="images">
+                        <h1>Opening Hours</h1>
+                        <p>Monday - Friday 09.00 - 18.00<br> Saturday 09.00 - 14.00
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+                    <div class="cs_contact_box" data-wow-duration="1.3s" style="visibility: visible; animation-duration: 0.8s; animation-name: slideInDown;">
+                        <img src="assets/images/index/SVG/Footer/phone.svg" class="img-fluid" alt="images">
+                        <h1>Call Us Anytime</h1>
+                        <p>(+91) 1800-214-122<br> (+91) 1800-214-123
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-12">
+                    <div class="cs_contact_box" data-wow-duration="1.3s" style="visibility: visible; animation-duration: 0.8s; animation-name: slideInRight;">
+                        <img src="assets/images/index/email.svg" class="img-fluid" alt="images">
+                        <h1>Mail Us</h1>
+                        <p>support@multifarious.com<br> support@courier.com
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--===================Footer Section==================-->
+    <div class="cs_footer_wrapper">
+        <div class="container">
+            <div class="row" data-wow-duration="1.3s" style="visibility: visible; animation-duration: 0.8s; animation-name: slideInDown;">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12">
+                    <div class="cs_footer_section1">
+                        <div class="cs_footer_logo">
+                            <a href="index.html"><img src="assets/images/index/logo.png" class="img-fluid" alt="images"> Multifarious</a>
+                        </div>
+                        <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                        <div class="cs_footer_icon">
+                            <h5>Follow Us</h5>
+                            <ul>
+                                <li><a href="javascript:void(0);"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="javascript:void(0);"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="javascript:void(0);"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="javascript:void(0);"><i class="fa fa-youtube-play"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12">
+                    <div class="cs_footer_section2">
+                        <h5>Courier Service</h5>
+                        <ul>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Standard</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Express</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;International</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Ware Housing</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Overnight</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Pallet</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="cs_footer_section3">
+                        <h5>Policies</h5>
+                        <ul>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Privacy Policy</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Terms of Use</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Mobile User</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Agreement</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Ad Choice</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Privacy</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="cs_footer_section4">
+                        <h5>Information</h5>
+                        <ul>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Express</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Material</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Furniture</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Vehicle</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Commodity</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Courier</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="cs_footer_section5">
+                        <h5>Company</h5>
+                        <ul>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;About</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Why Choose Us</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Contact Us</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Support</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Career</a></li>
+                            <li> <a href="javascript:void(0);">- &nbsp;&nbsp;Services</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cs_copyright">
+            <p>Copyright &copy; 2021 <a href="javascript:void(0);">Multifarious</a>. All Right Reserved.</p>
+        </div>
+    </div>
+    <!-- GO To Top -->
+    <a href="javascript:void(0);" id="scroll"><span class="fa fa-angle-double-up"></span></a>
+    <!-- Script Start -->
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/SmoothScroll.min.js"></script>
+    <script src="../assets/js/swiper.min.js"></script>
+    <script src="../assets/js/wow.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+</body>
+
+
+<!-- Mirrored from kamleshyadav.com/html/multifarious/html/courier-service/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 Jan 2024 05:51:11 GMT -->
+</html>
