523d61ac7345d06a03263065ed7fb5dd96b39635
Ashraf <miazi.ashraf@gmail.com>
2024-01-23 23:56:48 +0600

    resource con

---
+++ laravel-app/app/Http/Controllers/StdController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StdController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data=Student::all();
+        return view('stdList',compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('stdForm');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $r)
+    {
+        Student::create($r->all());
+        return redirect()->route('std.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $dt=Student::find($id);
+        return view('stdEdit',compact('dt'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Student::find($id)->delete();
+        return redirect()->route('std.index');
+    }
+}
